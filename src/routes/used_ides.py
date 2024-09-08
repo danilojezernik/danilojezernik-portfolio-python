909bac2f1751fb1bc0315c9cf9b83abf5fcbff34
Danilo Jezernik <dani.jezernik@gmail.com>
2024-09-08 08:56:06 +0200

    delete route

--- src/routes/used_ides.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://www.reddit.com/r/programming/search.json?q=IDE&restrict_sr=1'
-headers = {'User-Agent': 'Mozilla/5.0'}
-response = requests.get(url, headers=headers)
-data = response.json()
-
-print(data)
